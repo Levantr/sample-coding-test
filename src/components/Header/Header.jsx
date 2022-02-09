4032125f39d154693b1e132335f8f2f33191cdbb
kyle_socialive <kyle.cannon@socialive.us>
2022-02-08 19:34:49 -0800

    added changes

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.scss";
+
+import { Layout } from "antd";
+const { Header: AntHeader } = Layout;
+
+function Header() {
+  return (
+    <AntHeader className="Header">
+      <h1 className="Header__title">Levantr is the best!</h1>
+    </AntHeader>
+  );
+}
+
+export default Header;
